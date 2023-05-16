7574e8d9db3d20256287b8044d34486848427122
rafec <rafael.brizuena@gmail.com>
2023-05-16 19:55:30 -0400

    converting to typescript

--- initial-classes/index.ts
+++ initial-classes/index.ts
@@ -1,9 +1,9 @@
-"use strict";
-function sum(number1, number2) {
+function sum(number1: number, number2: number) {
     return number1 + number2;
-}
-;
+};
+
 const number1 = 10;
 const number2 = 5;
+
 const result = sum(number1, number2);
 console.log(result);
