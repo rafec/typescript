f453429af9009747941aebee7b6068bf0ac4d251
rafec <rafael.brizuena@gmail.com>
2023-05-22 20:12:28 -0400

    added classes examples

---
+++ initial-classes/classes/classes.ts
@@ -0,0 +1,11 @@
+class Data {
+    // public day: number;
+    // public month: number;
+    // public year: number;
+
+    constructor(public day: number, public month: number, public year: number = 1970) {
+        // this.day = day;
+        // this.month = month;
+        // this.year = year;
+    };
+};
