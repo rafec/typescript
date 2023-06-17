e2f73e6bf2ab4d846c9294e4f3e28c6176c78700
rafec <rafael.brizuena@gmail.com>
2023-06-16 21:14:42 -0400

    feat: add express initial server

--- apis/users-typescript-api/src/index.ts
+++ apis/users-typescript-api/src/index.ts
@@ -1,1 +1,11 @@
-console.log('hello world!');
+import express from "express";
+import { config } from "dotenv";
+
+config();
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
+app.get("/", (req, res) => {
+  res.send("hello world!");
+});
